42370305818748fbe0cd8fcca32163180ddd67a3
Augusto Moscardo <augustomsaluz@gmail.com>
2025-07-17 16:15:53 -0300

    testcontainer

--- test/mathUtils.test.js
+++ test/mathUtils.test.js
@@ -1,5 +1,25 @@
 import { expect } from "chai"
+import { GenericContainer } from "testcontainers"
 import { sum, divide } from "../src/mathUtils.js"
+
+let container;
+
+before(async function () {
+  this.timeout(30000)
+
+  container = await new GenericContainer("alpine")
+    .withCommand(["sh", "-c", "sleep 60"])
+    .start();
+
+  console.log(`Container iniciada: ${container.getId()}`);
+})
+
+after(async function () {
+  if (container) {
+    await container.stop()
+    console.log('Container encerrada.');
+  }
+})
 
 describe('sum', () => {
   it('should add two numbers', () => {
